471c0f090ea2f9d12e0c51da57295d99783724ed
Michael Primavera <pastasauce59@gmail.com>
2022-06-16 22:30:15 -0400

    created MentalState component, set up working route and links to navigate from landing page to MentalState, added css styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import React, { useState } from 'react'
+import {Routes, Route, Link} from 'react-router-dom'
+import MentalState from './MentalState';
 
 function App() {
   return (
     <div className="App">
-     <header>
-         <h1>Choose Your ~State of Mind~</h1>
-      </header>
+      <Routes>
+        <Route exact path='/' element={
+        <div>
+          <header>
+            <h1>Choose Your ~State of Mind~</h1>
+          </header>
+
+          <div className='choices'>
+            <Link to='mental-state' className='choice'>Focus</Link>
+            <Link to='mental-state' className='choice'>Relax</Link>
+            <Link to='mental-state' className='choice'>Sleep</Link>
+          </div>
+        </div>
+        } />
+
+        <Route exact path='/mental-state' element={<MentalState />} />
+
+      </Routes>
     </div>
   );
 }
---
+++ src/MentalState.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MentalState(props) {
+    return (
+        <div>
+           <h1>Inside Mental State</h1>
+        </div>
+    );
+}
+
+export default MentalState;
