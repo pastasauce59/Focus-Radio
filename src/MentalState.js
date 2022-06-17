cd62355d4273c4a5cb375882a33263fcdca51b7e
Michael Primavera <pastasauce59@gmail.com>
2022-06-16 23:24:23 -0400

    created a count state and handle previous and next functions  to traverse through audio props from app.js

--- src/MentalState.js
+++ src/MentalState.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
 
 function MentalState(props) {
+    const [count, setCount] = useState(0)
 
-    console.log(props.audio)
+    let handlePrev = () => {
+        if(count - 1 < 0){
+            return
+        }
+        setCount(count - 1)
+    }
 
+    let handleNext = () => {
+        if(count + 1 >= props.audio.audio.length){
+            return
+        }
+        setCount(count + 1)
+    }
+
+
+    console.log(props.audio.audio[count].src)
     return (
         <div>
             <div className='home'>
             <Link to='/'>Home/Re-Focus</Link>
             </div>
 
-           <h1>Inside Mental State</h1>
+            <h1>Current Mental State: {props.audio.state}</h1>
+            <h4>Current Song: {props.audio.audio[count].title}</h4>
+            <audio controls autoplay='autoplay'>
+                <source src={props.audio.audio[count].src} type="audio/mpeg"></source>
+                Audio is not supported in your web browser.
+            </audio>
+            <div className='prev-next-btns'>
+                <h4 onClick={handlePrev} className='prev-next'>⏪</h4>
+                <h4 onClick={handleNext} className='prev-next'>⏩</h4>
+            </div>
         </div>
     );
 }
