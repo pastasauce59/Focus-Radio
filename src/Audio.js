1d5e03ddff5c6a5c48909ec83f1d992ea090beec
Michael Primavera <pastasauce59@gmail.com>
2022-06-16 22:47:41 -0400

    added assets folder to public folder with audio files, created Audio component to store refrences to audio files in objects

---
+++ src/Audio.js
@@ -0,0 +1,20 @@
+const Focus = [
+
+    {title: 'Aspen Migration', src:'/assets/focus/aspen_migration.mp3'},
+    {title: 'Cyan', src:'/assets/focus/cyan.mp3'},
+    {title: 'Treasure Map', src:'/assets/focus/treasure_map.mp3'}
+]
+
+const Relax = [
+    {title: 'Bathed In Neon', src:'/assets/relax/bathed_in_neon.mp3'},
+    {title: 'Low Tide', src:'/assets/relax/low_tide.mp3'},
+    {title: 'Vapor', src:'/assets/relax/vapor.mp3'}
+]
+
+const Sleep = [
+    {title: 'Dark Moon', src:'/assets/sleep/dark_moon.mp3'},
+    {title: 'Lunaris', src:'/assets/sleep/lunaris.mp3'},
+    {title: 'Moonflower', src:'/assets/sleep/moonflower.mp3'}
+]
+
+export { Focus, Relax, Sleep}
