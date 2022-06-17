a60ea86b498a400986de42f356dbb608e78781b9
Michael Primavera <pastasauce59@gmail.com>
2022-06-16 22:35:11 -0400

    created a button in MentalState component to navigate back home and added styling

--- src/MentalState.js
+++ src/MentalState.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function MentalState(props) {
     return (
         <div>
+            <div className='home'>
+            <Link to='/'>Home/Re-Focus</Link>
+            </div>
+
            <h1>Inside Mental State</h1>
         </div>
     );
